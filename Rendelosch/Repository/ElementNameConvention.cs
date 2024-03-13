04f10cb2dfdbe4cc9e03843702ba83ee3f43a3fc
Laci <mlacko020523@gmail.com>
2024-03-13 19:06:12 +0100

    ElementNameConvention

---
+++ Rendelosch/Repository/ElementNameConvention.cs
@@ -0,0 +1,39 @@
+﻿using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Conventions;
+
+namespace Rendelosch.Repository;
+
+public class ElementNameConvention : CamelCaseElementNameConvention, IMemberMapConvention
+{
+    void IMemberMapConvention.Apply(BsonMemberMap memberMap)
+    {
+        string name = memberMap.MemberName;
+        var firstLowercaseIndex = FindFirstLowercaseIndex(name);
+
+        switch (firstLowercaseIndex)
+        {
+            case < 0:
+                memberMap.SetElementName(name.ToLowerInvariant());
+                break;
+            case > 1:
+                memberMap.SetElementName(name.Substring(0, firstLowercaseIndex - 1).ToLowerInvariant() + name.Substring(firstLowercaseIndex - 1));
+                break;
+            default:
+                Apply(memberMap);
+                break;
+        }
+    }
+
+    private static int FindFirstLowercaseIndex(string name)
+    {
+        for (int i = 0; i < name.Length; i++)
+        {
+            if (char.IsLower(name, i))
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
