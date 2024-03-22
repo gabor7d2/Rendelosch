8098bbab7ad03180ee3bcb03202ece2104a2f0fa
László Mócsy <laszlomocsy.dev@gmail.com>
2024-03-22 10:05:09 +0200

    Finished `CreateForm` page (#1)
    * Add `OnPost()` handling to `CreateForm`
    
    * Redesign page UI/UX
    
    * Add JS script to the `CreateForm` page
    
    * Form submit now sends data as `POST` request
    
    * Finished `CreateForm.OnPost()` method

--- Rendelosch/Pages/CreateForm.cshtml.cs
+++ Rendelosch/Pages/CreateForm.cshtml.cs
@@ -1,11 +1,36 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Rendelosch.Models;
+using Rendelosch.Repository;
 
 namespace Rendelosch.Pages;
 
 public class CreateForm : PageModel
 {
-    public void OnGet()
+    public IProductFormRepository Repository { get; }
+    
+    public CreateForm(IProductFormRepository repository)
     {
+        Repository = repository;
+    }
+    
+    public void OnPost()
+    {
+        Request.Form.TryGetValue("form_title", out var title);
+        Request.Form.TryGetValue("form_from_date", out var fromDate);
+        Request.Form.TryGetValue("form_from_time", out var fromTime);
+        Request.Form.TryGetValue("form_to_date", out var toDate);
+        Request.Form.TryGetValue("form_to_time", out var toTime);
+        Request.Form.TryGetValue("form_fields", out var fields);
         
+        List<Field> fieldsList = [];
+        foreach (var fieldString in fields.ToString().Split(';'))
+        {
+            var field = fieldString.Split(':');
+            fieldsList.Add(new Field(field[0], field[1]));
+        }
+
+        Repository.CreateProductForm(title.ToString(), fieldsList);
+        
+        Response.Redirect("/");
     }
 }
